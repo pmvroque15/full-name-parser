cb68a5e0660b420ee4346afa8aefb32f10ddf209
pmvroque15 <pmvroque@gmail.com>
2026-04-14 09:44:01 -0400

    Finished exercise

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -10,12 +10,19 @@ public class Main {
 
         System.out.println("Please enter your full name");
         String userFullName = scanner.nextLine();
+        userFullName = userFullName.trim();
+        String[] nameSplit = userFullName.split("\\s+"); // \\s is an escape character and "+" is one or more whitespace
 
-        String[] nameSplit = userFullName.split(" ");
-
-        System.out.printf("First name: %s\n", nameSplit[0]);
-        System.out.printf("Middle name: %s\n", nameSplit[1]);
-        System.out.printf("Last name: %s\n", nameSplit[2]);
+        //If the user did not add middle name
+        if (nameSplit.length == 2) {
+            System.out.printf("First name: %s\n", nameSplit[0]);
+            System.out.println("Middle name: ");
+            System.out.printf("Last name: %s\n", nameSplit[1]);
+        } else if ( nameSplit.length == 3) {
+            System.out.printf("First name: %s\n", nameSplit[0]);
+            System.out.printf("Middle name: %s\n", nameSplit[1]);
+            System.out.printf("Last name: %s\n", nameSplit[2]);
+        }
     }
 }
 
